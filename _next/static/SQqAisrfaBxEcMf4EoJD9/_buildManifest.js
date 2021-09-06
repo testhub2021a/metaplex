a646ff1fae4bcfad6c1db258ad2b25515b7d6a0a
T <testhub2021@protonmail.com>
2021-09-06 13:54:26 -0700

    Updates

---
+++ _next/static/SQqAisrfaBxEcMf4EoJD9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-67396127cbe532d2dc9b.js"],"/_error":["static/chunks/pages/_error-e1bcaa043b7e8bc8fc85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
